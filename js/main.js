f4ec23ddf8163b613c32c4dce49d135da6ad59ee
silverapm <disfrom21@gmail.com>
2021-07-06 11:21:47 +0300

    add js funcs

---
+++ js/main.js
@@ -0,0 +1,103 @@
+document.getElementById('button1').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[0].style= "display: none";
+    document.getElementsByClassName('bigphoneblock')[1].style= "display: none";
+    document.getElementsByClassName('bigphoneblock')[2].style= "display: none";
+    document.getElementsByClassName('bigphoneblock')[3].style= "display: none";
+    document.getElementsByClassName('bigphoneblock')[4].style= "display: none";
+    document.getElementsByClassName('bigphoneblock')[5].style= "display: none";
+    document.getElementsByClassName('bigphoneblock')[6].style= "display: none";
+    document.getElementsByClassName('bigphoneblock')[7].style= "display: none";
+}
+
+document.getElementById('pagebtn1').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[0].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[1].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[2].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[3].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[4].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[5].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[6].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[7].style= "display: block";
+}
+
+document.getElementById('pagebtn2').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[0].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[1].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[2].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[3].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[4].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[5].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[6].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[7].style= "display: block";
+}
+
+document.getElementById('pagebtn3').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[0].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[1].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[2].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[3].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[4].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[5].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[6].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[7].style= "display: block";
+}
+
+document.getElementById('pagebtn4').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[0].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[1].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[2].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[3].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[4].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[5].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[6].style= "display: block";
+    document.getElementsByClassName('bigphoneblock')[7].style= "display: block";
+}
+
+document.getElementById('removebtn0').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[0].style= "display: none";
+}
+
+document.getElementById('removebtn1').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[1].style= "display: none";
+}
+
+document.getElementById('removebtn2').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[2].style= "display: none";
+}
+
+document.getElementById('removebtn3').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[3].style= "display: none";
+}
+
+document.getElementById('removebtn4').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[4].style= "display: none";
+}
+
+document.getElementById('removebtn5').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[5].style= "display: none";
+}
+
+document.getElementById('removebtn6').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[6].style= "display: none";
+}
+
+document.getElementById('removebtn7').onclick = function()
+{
+    document.getElementsByClassName('bigphoneblock')[7].style= "display: none";
+}
+
+
+
+
