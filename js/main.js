b6b88b2c1c8a959aa7a1063aee65e68b1fa89b51
silverapm <disfrom21@gmail.com>
2021-07-08 23:41:24 +0300

    fixed bugs, add correct buttons

--- js/main.js
+++ js/main.js
@@ -1,70 +1,18 @@
-
-document.getElementById('removebtn0').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 0);
-}
-
-document.getElementById('removebtn1').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 1);
-}
-
-document.getElementById('removebtn2').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 2);
-}
-
-document.getElementById('removebtn3').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 3);
-}
-
-document.getElementById('removebtn4').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 4);
-}
-
-document.getElementById('removebtn5').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 5);
-}
-
-document.getElementById('removebtn6').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 6);
-}
-
-document.getElementById('removebtn7').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 7);
-}
-
-document.getElementById('removeAbtn0').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 0);
-}
-
-document.getElementById('removeAbtn1').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 1);
-}
-
-document.getElementById('removeAbtn2').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 2);
-}
-
-document.getElementById('removeAbtn3').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 3);
-}
-
-document.getElementById('removeAbtn4').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 4);
-}
-
-document.getElementById('removeAbtn5').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 5);
-}
-
-document.getElementById('removeAbtn6').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 6);
-}
-
-document.getElementById('removeAbtn7').onclick = function () {
-    setStyleByClassName('bigphoneblock', 'display: none', 7);
-}
-
 let searchResults = null;
 const pageSize = 8;
+let pageNumber = 0;
+
+function removeHandler() {
+    const elements = document.querySelectorAll('.removeButton');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].onclick = function () {
+            let phoneBlock = this.closest('.bigphoneblock');
+            phoneBlock.classList.add("deleted");
+            paginate();
+            generatePagination();
+        }
+    }
+}
 
 function setStyleByClassName(className, style, index) {
     const elements = document.getElementsByClassName(className);
@@ -88,7 +36,7 @@ function search() {
         searchResults = null;
     } else {
         searchResults = [];
-        const elements = document.getElementsByClassName('bigphoneblock');
+        const elements = document.querySelectorAll('.bigphoneblock:not(.deleted)');
 
         for (let i = 0; i < elements.length; i++) {
             const title = elements[i].getElementsByTagName('h4')[0];
@@ -99,7 +47,8 @@ function search() {
             }
         }
     }
-    paginate(0);
+    pageNumber = 0;
+    paginate();
     generatePagination();
 }
 
@@ -116,43 +65,41 @@ function search() {
                 }
             }, false);
         });
-        paginate(0);
+        pageNumber = 0;
+        removeHandler();
+        paginate();
         generatePagination();
     }, false);
 })();
 
-function paginate(page) {
+function paginate() {
     let style;
-    const elements = searchResults||document.getElementsByClassName('bigphoneblock');
+    const elements = searchResults || document.querySelectorAll('.bigphoneblock:not(.deleted)');
 
     for (let i = 0; i < elements.length; i++) {
-        if (page*pageSize <= i && i < page*pageSize+pageSize) {
+        if (pageNumber * pageSize <= i && i < pageNumber * pageSize + pageSize) {
             style = 'display: block';
         } else {
             style = 'display: none';
         }
-        elements[i].style=style;
+        elements[i].style = style;
     }
 }
 
 
-function generatePagination () {
-    const elements = searchResults||document.getElementsByClassName('bigphoneblock');
+function generatePagination() {
+    const elements = searchResults || document.querySelectorAll('.bigphoneblock:not(.deleted)');
     const pagination = document.getElementById('pagination');
     pagination.innerHTML = '';
 
-    for (let i = 0; i < Math.ceil(elements.length/pageSize); i++) {
-        pagination.innerHTML += '<button type="button" class="btn btn-secondary btn-lg pagebtn" id="pagebtn'+i+'">'+(i+1)+'</button>';
-        let item = document.getElementById('pagebtn'+i);
-        /*
-        document.getElementById('pagebtn'+i).onclick = function () {
-            paginate(i);
-        }
-         */
+    for (let i = 0; i < Math.ceil(elements.length / pageSize); i++) {
+        pagination.innerHTML += '<button type="button" class="btn btn-secondary btn-lg pagebtn" id="pagebtn' + i + '">' + (i + 1) + '</button>';
     }
-    for (let i = 0; i < elements.length; i++) {
-        document.getElementById('pagebtn'+i).onclick = function () {
-            paginate(i);
+
+    for (let i = 0; i < Math.ceil(elements.length / pageSize); i++) {
+        document.getElementById('pagebtn' + i).onclick = function () {
+            pageNumber = i;
+            paginate();
         }
     }
 }
@@ -160,7 +107,8 @@ function generatePagination () {
 document.getElementById('clearlistbtn').onclick = function () {
     setStyleByClassName('bigphoneblock', 'display: none');
     searchResults = [];
-    paginate(0);
+    pageNumber = 0;
+    paginate();
     generatePagination();
     document.getElementById("clearlistbtn").disabled = true;
     document.getElementById("inputSearch").disabled = true;
